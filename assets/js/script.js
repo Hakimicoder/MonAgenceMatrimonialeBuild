ae92031f6e1a6e367ce465282ba47f18d8f28b38
Hakimicoder <you@example.com>
2025-07-20 19:52:33 +0100

    ajout du footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,10 @@
-// const slider = document.getElementById('slider');
-// const nextBtn = document.getElementById('nextSlide');
-// const prevBtn = document.getElementById('prevSlide');
-
-// const slides = slider.children;
-// let currentIndex = 0;
-
-// const updateSlideWidth = () => slides[0].offsetWidth;
-
-// const goToSlide = (index) => {
-//     const slideWidth = updateSlideWidth();
-//     slider.style.transform = `translateX(-${slideWidth * index}px)`;
-// };
-
-// const goToNext = () => {
-//     currentIndex++;
-//     if (currentIndex >= slides.length) {
-//         currentIndex = 0;
-//     }
-//     goToSlide(currentIndex);
-// };
-
-// const goToPrev = () => {
-//     currentIndex--;
-//     if (currentIndex < 0) {
-//         currentIndex = slides.length - 1;
-//     }
-//     goToSlide(currentIndex);
-// };
-
-// // Boutons manuels
-// nextBtn.addEventListener('click', () => {
-//     goToNext();
-//     resetAutoplay(); // reset timer on click
-// });
-
-// prevBtn.addEventListener('click', () => {
-//     goToPrev();
-//     resetAutoplay(); // reset timer on click
-// });
-
-// // Resize
-// window.addEventListener('resize', () => {
-//     goToSlide(currentIndex);
-// });
-
-// // AutoPlay
-// let autoplayInterval = null;
-
-// const startAutoplay = () => {
-//     autoplayInterval = setInterval(goToNext, 4000); // 4 secondes
-// };
-
-// const resetAutoplay = () => {
-//     clearInterval(autoplayInterval);
-//     startAutoplay();
-// };
-
-// startAutoplay();
-
-
-
 const slider = document.getElementById('slider');
 const nextBtn = document.getElementById('nextSlide');
 const prevBtn = document.getElementById('prevSlide');
+const pFooter = document.getElementById('pFooter');
+
+const date = 2025;
+pFooter.innerText = `${date} Afromeet - Alls rights reserved.`;
 
 const slides = slider.children;
 let currentIndex = 0;
